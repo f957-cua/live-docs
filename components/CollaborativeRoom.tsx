ca912e1fa5f8c38fc5cd9fbb52b467283806c05d
f957-cua <ivanfiliptschik00@gmail.com>
2024-09-16 13:51:35 +0300

    add list of rooms on the landing

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,6 +10,7 @@ import { Input } from './ui/input';
 import Image from 'next/image';
 import { Smokum } from 'next/font/google';
 import { updateDocument } from '@/lib/actions/room.actions';
+import Loader from './Loader';
 
 const CollaborativeRoom = ({
   roomId,
@@ -71,7 +72,7 @@ const CollaborativeRoom = ({
 
   return (
     <RoomProvider id={roomId}>
-      <ClientSideSuspense fallback={<div>Loading…</div>}>
+      <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
             <div
