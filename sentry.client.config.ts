5dca7fb5b6339669f1f77f89d82317e6e1dfae6b
f957-cua <ivanfiliptschik00@gmail.com>
2024-09-24 23:21:00 +0300

    making app secure ADD SENTRY

---
+++ sentry.client.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0733c1db7300b84dcf2a27022b9ecae0@o4508009234563072.ingest.de.sentry.io/4508009243541584",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
